53e0a2a51f392421d29007924d8151694f663355
Dani <buccadany@gmail.com>
2025-11-16 02:23:20 +0100

    Aggiornata mappa e box filtri con stile coerente alla sidebar

---
+++ dist/assets/MapWrapper-9b801b65.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-6f8699be.js";const s=({children:r})=>e.jsx("div",{className:"w-full h-[80vh] md:h-[600px] rounded-xl overflow-hidden shadow-lg",children:r});export{s as default};
